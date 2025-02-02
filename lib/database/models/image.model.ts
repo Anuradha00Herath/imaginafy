abe538ca2b8b436b9e84ed7fa6a20eda60b8fc21
Anuradha00Herath <herathanuradha03@gmail.com>
2025-02-02 22:15:22 +0530

    Update dependancies and fix UI issues

--- lib/database/models/image.model.ts
+++ lib/database/models/image.model.ts
@@ -1,10 +1,11 @@
 import { model, models, Schema } from "mongoose";
 
 export interface IImage extends Document {
+    _id:string;
     title: string;
     transformationType: string;
     publicId: string;
-    secureUrl: string;
+    secureURL: string;
     width?: number;
     height?: number;
     config?: object;
@@ -26,11 +27,11 @@ const ImageSchema = new Schema({
     title:{type:String, required:true},
     transformationType : {type:String, required: true},
     publicId:{type:String, required:true},
-    secureUrl:{type:URL, required:true},
+    secureURL:{type:String, required:true},
     width:{type:Number},
     height:{type:Number},
     config:{type:Object},
-    transformationUrl:{type:URL},
+    transformationUrl:{type:String},
     aspectRatio:{type:String},
     color:{type:String},
     prompt:{ type:String},
